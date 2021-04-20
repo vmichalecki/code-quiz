93dcf86436ba645564cab8c833adb05658196377
Valerie Michalecki <valerie227@gmail.com>
2021-04-19 21:07:58 -0500

    created a questions object that contains all the questions and answers, started the ffunction to verify answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,111 @@
-let question = document.getElementById('question');
-let choice1 = document.getElementById('choice-1');
-let choice2 = document.getElementById('choice-2');
-let choice3 = document.getElementById('choice-3');
-let choice4 = document.getElementById('choice-4');
+// page loads
+// link to high scores from local storage is loaded on the top
+// start button with click event listender initiates the game
+// when clicked, timer starts and first question/set of answers displays and instructions disappear
+// each answer is a button with a click event listener
+// when the user chooses an answer, a function evaluates if the answer is correct or not
+// if the answer is correct, increase score, display 'correct', and load the next question
+// if the answer is incorrect, do nothing to the score, subtract time from the clock, display 'incorrect', and load the next question
+// if all questions are answered before time is up, then score is displayed with form to enter initials which are stored in local storage upon the click of a button with an event listener, and a button with an event listener listening for a click to restart the game appears
+// if the timer runs out before the end of the game, then score is displayed with form to enter initials which are stored in local storage upon the click of a button with an event listener, and a button with an event listener listening for a click to restart the game appears
 
-let questions = {
-	question: "What's your favorite color?",
-	choice1: 'rebecca purple',
-	choice2: 'lemon chiffon',
-	choice3: 'hot pink',
-	choice4: 'alice blue',
-};
-console.log(questions);
+let body = document.body;
+let quiz = document.createElement('div');
+let h2El = document.createElement('h2');
+let choice1 = document.createElement('button');
+let choice2 = document.createElement('button');
+let choice3 = document.createElement('button');
+let choice4 = document.createElement('button');
 
-// write a function to load a question and subsequent choices
+const questionIndex = 0;
 
-choice1.addEventListener('click', function () {});
-choice2.addEventListener('click', function () {});
-choice3.addEventListener('click', function () {});
-choice4.addEventListener('click', function () {});
+body.appendChild(quiz);
+quiz.appendChild(h2El);
+quiz.appendChild(choice1);
+quiz.appendChild(choice2);
+quiz.appendChild(choice3);
+quiz.appendChild(choice4);
 
-// write function to get highest score
-// write function to start the timer counting down
-function init() {
-	getHighestScore();
+const questions = [
+    {
+        num: 'question 1',
+        q: "what's your favorite color?",
+        a: 'rebecca purple',
+        b: 'lemon chiffon',
+        c: 'hot pink',
+        d: 'alice blue',
+        correct: 'hot pink',
+    },
+    {
+        num: 'question 2',
+        q: "what's your favorite color?",
+        a: 'rebecca purple',
+        b: 'lemon chiffon',
+        c: 'hot pink',
+        d: 'alice blue',
+        correct: 'hot pink',
+    },
+    {
+        num: 'question 3',
+        q: "what's your favorite color?",
+        a: 'rebecca purple',
+        b: 'lemon chiffon',
+        c: 'hot pink',
+        d: 'alice blue',
+        correct: 'hot pink',
+    },
+    {
+        num: 'question 4',
+        q: "what's your favorite color?",
+        a: 'rebecca purple',
+        b: 'lemon chiffon',
+        c: 'hot pink',
+        d: 'alice blue',
+        correct: 'hot pink',
+    },
+    {
+        num: 'question 5',
+        q: "what's your favorite color?",
+        a: 'rebecca purple',
+        b: 'lemon chiffon',
+        c: 'hot pink',
+        d: 'alice blue',
+        correct: 'hot pink',
+    },
+];
+
+function showQuestion() {
+    h2El.innerHTML = questions.q;
+    choice1.innerHTML = questions.a;
+    choice2.innerHTML = questions.b;
+    choice3.innerHTML = questions.c;
+    choice4.innerHTML = questions.d;
+
+    choice1.addEventListener('click', verifyAnswer);
+    choice2.addEventListener('click', verifyAnswer);
+    choice3.addEventListener('click', verifyAnswer);
+    choice4.addEventListener('click', verifyAnswer);
+    //loop through array of objects
+    let setQuestion = questions[questionIndex];
+    for (let i = 0; i < questions.length; i++);
 }
 
-// function to check if choice is correct, displays correct or incorrect briefly, loads next question
+function verifyAnswer() {
+    const alertCorrect = alert('well done');
+    const alertIncorrect = alert('you suck');
+    if (questions.a || questions.b || questions.c || questions.d === questions.correct) {
+        alertCorrect;
+    } else {
+        alertIncorrect;
+    }
+}
 
-// event listener listening for click on one of the choices
+console.log(body);
 
-// when start button is clicked, the timer starts and the first questions loads
+// write function that loads high scores and sets tinmer to 0 when page loads
 
-// event listener listening for click on start button
+function init() {
+    window;
+}
 
-// page loads and high scores from local storage display in top left and timer displays in top right
-startButton.addEventListener('click', startGame);
-
-// use init to start the page
-init();
+// init();
