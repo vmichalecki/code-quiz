a181afe711b1ad876c8bbbaa5ea22f629dc19f7d
Valerie Michalecki <valerie227@gmail.com>
2021-04-25 23:47:09 -0500

    added comments to secondary js file

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,6 +1,8 @@
+// duplicated variables from script.js
 let highScores = document.getElementById('high-scores');
 let highScoresArray = JSON.parse(localStorage.getItem('highScoresArray')) || [];
 
+// displays top 10 high scores sorted from highest to lowest
 function displayScores() {
 	highScoresArray.sort((a, b) => b.score - a.score);
 	highScoresArray.splice(10);
